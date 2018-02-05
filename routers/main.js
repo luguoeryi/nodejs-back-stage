9caadf64e98f08928aebc344c3a10087fa8a841a
jingkong <1099064949@qq.com>
2018-02-05 18:28:28 +0800

    '18-1-5'

--- routers/main.js
+++ routers/main.js
@@ -4,16 +4,13 @@
 let express = require('express');
 let router = express.Router();
 
-/**
-app.get('/', function (req, res, next) {
-
+router.get('/', function (req, res, next) {
     /**
     * 读取views 目录下的指定文件，解析并返回给客户端
     * @param telFile 模板的文件，相对于views目录
     * @param data 传递给模板使用的数据
-    *
-    // res.render('index');
+    */
+    res.render('index');
 });
-*/
 
 module.exports = router;
