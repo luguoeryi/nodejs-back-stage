a57cc71c01c81496778f721c2557c1e48618176c
luguoeryi <1099064949@qq.com>
2018-04-11 01:37:39 +0800

    '修改删除后台分类'

--- routers/admin.js
+++ routers/admin.js
@@ -52,7 +52,8 @@ router.get('/user', function (req, res, next) {
                 count: count,
                 limit: limit,
                 pages: pages,
-                page: page
+                page: page,
+                changePageUrl: '/admin/user'
             });
         });
     });
@@ -62,8 +63,31 @@ router.get('/user', function (req, res, next) {
  * 分类管理
 */
 router.get('/category', function (req, res, next) {
-    res.render('admin/category_index', {
-        userInfo: req.userInfo
+    let page = Number(req.query.page) || 1;
+    let limit = 2;
+    let pages = 0;
+
+    Category.count().then(function (count) {
+        // 计算总页数
+        pages = Math.ceil(count / limit);
+        // 取值不能超过pages
+        page = Math.min(page, pages);
+        // 取值不能小于1
+        page = Math.max(page, 1);
+        
+        let skip = (page - 1) * limit;
+
+        Category.find().limit(limit).skip(skip).then(function (categories) {
+            res.render('admin/category_index', {
+                userInfo: req.userInfo,
+                categories: categories,
+                count: count,
+                limit: limit,
+                pages: pages,
+                page: page,
+                changePageUrl: '/admin/category'
+            });
+        });
     });
 });
 
@@ -117,6 +141,109 @@ router.post('/category/add', function (req, res, next) {
     });
 });
 
+/**
+ * 分类的修改
+*/
+router.get('/category/edit', function (req, res, next) {
+    let id = req.query.id || '';
+    if (!id) {
+        res.render('admin/error', {
+            userInfo: req.userInfo,
+            message: '修改提交有误！'
+        });
+        return;
+    }
 
+    // 获取要修改的分类信息
+    Category.findOne({
+        _id: id
+    }).then(function (category) {
+        if (!category) {
+            res.render('admin/error', {
+                userInfo: req.userInfo,
+                message: '分类信息不存在！'
+            });
+        } else {
+            res.render('admin/category_edit', {
+                userInfo: req.userInfo,
+                category: category
+            });
+        }
+    });
+});
+
+/**
+ * 分类的保存
+*/
+router.post('/category/edit', function (req, res, next) {
+    let id = req.query.id || '';
+    let name = req.body.name || '';
+
+    Category.findOne({
+        _id: id
+    }).then(function (category) {
+        if (!category) {
+            res.render('admin/error', {
+                userInfo: req.userInfo,
+                message: '分类信息不存在！'
+            });
+            return Promise.reject();
+        } else {
+            if (name == category.name) {
+                res.render('admin/error', {
+                    userInfo: req.userInfo,
+                    message: '您未修改分类名称！'
+                });
+                return Promise.reject();
+            } else {
+                // 要修改的名称是否在数据库中已经存在
+                return Category.findOne({
+                    _id: {$ne: id},
+                    name: name
+                });
+            }
+        }
+    }).then(function (sameCategory) {
+        if (sameCategory) {
+            res.render('admin/error', {
+                userInfo: req.userInfo,
+                message: '该分类名称已经存在！'
+            });
+            return Promise.reject();
+        } else {
+            /**
+             * @param {json} 条件
+             * @param {json} 值
+            */
+            return Category.update({
+                _id: id
+            }, {
+                name: name
+            });
+        }
+    }).then(function () {
+        res.render('admin/success', {
+            userInfo: req.userInfo,
+            message: '修改成功！',
+            url: '/admin/category'
+        });
+    });
+});
+
+/**
+ * 分类的删除
+*/
+router.get('/category/delete', function (req, res, next) {
+    let id = req.query.id;
+    Category.remove({
+        _id: id
+    }).then(function () {
+        res.render('admin/success', {
+            userInfo: req.userInfo,
+            message: '删除成功！',
+            url: '/admin/category'
+        });
+    });
+});
 
 module.exports = router;
