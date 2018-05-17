186711c6a0c65eea5b9613d02e42b27d7f9634c8
luguoeryi <1099064949@qq.com>
2018-05-17 23:35:09 +0800

    '嵌套首页模板'

--- routers/main.js
+++ routers/main.js
@@ -4,19 +4,49 @@
 let express = require('express');
 let router = express.Router();
 let Category = require('../models/Category');
+let Content = require('../models/Content');
 
+/**
+ * 首页
+*/
 router.get('/', function (req, res, next) {
-    /**
-    * 读取views 目录下的指定文件，解析并返回给客户端
-    * @param telFile 模板的文件，相对于views目录
-    * @param data 传递给模板使用的数据
-    */
-
+    let page = req.query.page || 1
+    let data = {
+        userInfo: req.userInfo,
+        categories: [],
+        contents: [],
+        count: 0,
+        page: page,
+        limit: 2,
+        pages: 0
+    };
+    
     // 读取所有的分类信息
     Category.find().then(function (categories) {
-        res.render('main/index', {
-            userInfo: req.userInfo,
-            categories: categories || []
+
+        data.categories = categories;
+
+        return Content.count();
+
+    }).then(function (count) {
+
+        data.count = count;
+
+        data.pages = Math.ceil(data.count / data.limit);
+
+        data.page = Math.min(data.page, data.pages);
+
+        data.page = Math.max(data.page, 1);
+
+        let skip = (data.page - 1) * data.limit;
+
+        return Content.find().limit(data.limit).skip(skip).populate(['category', 'user']);
+
+    }).then(function (contents) {
+        data.contents = contents;
+        console.log(data);
+        res.render('main/index2', {
+            data: data
         });
     });
 });
