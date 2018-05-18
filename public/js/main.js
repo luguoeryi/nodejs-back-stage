81b0d05636b598e109e9ee4165fac40fcb9f8efb
luguoeryi <1099064949@qq.com>
2018-05-18 23:10:59 +0800

    '首页分页处理及导航栏的高亮显示'

---
+++ public/js/main.js
@@ -0,0 +1,92 @@
+
+$(window).ready(function () {
+    reg();
+    login();
+    logout();
+
+    $('#showLoginBtn').on('click', function () {
+        var oFormLogin = $('.form-wrap-login')[0];
+        var oFormReg = $('.form-wrap-reg')[0];
+        $(oFormLogin).show();
+        $(oFormReg).hide();
+    });
+    $('#showRegBtn').on('click', function () {
+        var oFormLogin = $('.form-wrap-login')[0];
+        var oFormReg = $('.form-wrap-reg')[0];
+        $(oFormLogin).hide();
+        $(oFormReg).show();
+    });
+});
+
+// 注册
+function reg () {
+    var aRegBtn = $('#btn-reg');
+    var oFormReg = $('.form-wrap-reg')[0];
+    var oInfoStatus = $('.form-wrap-reg .info-status')[0];
+    var oFormLogin = $('.form-wrap-login')[0];
+    $(aRegBtn).on('click', function () {
+        $.ajax({
+            type: 'post',
+            url: '/api/user/register',
+            data: {
+                userName: $(oFormReg).find('[name="username"]').val(),
+                passWord: $(oFormReg).find('[name="password"]').val(),
+                rePassword: $(oFormReg).find('[name="repassword"]').val()
+            },
+            dataType: 'json',
+            success: function (result) {
+                if (result.status == 200) {
+                    setTimeout(function () {
+                        $(oFormReg).hide();
+                        $(oFormLogin).show();
+                    }, 1000);
+                } else {
+                    $(oInfoStatus).text(result.message);
+                }
+            }
+        });
+    });
+}
+
+// 登录
+function login () {
+    var aLoginBtn = $('#btn-login');
+    var oFormLogin = $('.form-wrap-login')[0];
+    var oInfoStatus = $('.form-wrap-login .info-status')[0];
+    $(aLoginBtn).on('click', function () {
+        $.ajax({
+            type: 'post',
+            url: '/api/user/login',
+            data: {
+                userName: $(oFormLogin).find('[name="username"]').val(),
+                passWord: $(oFormLogin).find('[name="password"]').val()
+            },
+            dataType: 'json',
+            success: function (result) {
+                if (result.status == 200) {
+
+                    window.location.reload();
+                    
+                } else {
+
+                    $(oInfoStatus).text(result.message);
+
+                }
+            }
+        });
+    }); 
+}
+
+// 退出
+function logout () {
+    $('#logout,#adminLogout').on('click', function () {
+        $.ajax({
+            url: '/api/user/logout',
+            success: function (result) {
+                if (result.status == 200) {
+                    window.location.reload();
+                }
+            }
+        });
+    });
+}
--- routers/main.js
+++ routers/main.js
@@ -11,22 +11,30 @@ let Content = require('../models/Content');
 */
 router.get('/', function (req, res, next) {
     let page = req.query.page || 1
+    let category = req.query.category || ''
     let data = {
         userInfo: req.userInfo,
         categories: [],
         contents: [],
+        category: category,
         count: 0,
         page: page,
-        limit: 2,
+        limit: 1,
         pages: 0
     };
+
+    let where = {};
+
+    if (category) {
+        where.category = data.category;
+    }
     
     // 读取所有的分类信息
     Category.find().then(function (categories) {
 
         data.categories = categories;
 
-        return Content.count();
+        return Content.where(where).count();
 
     }).then(function (count) {
 
@@ -40,7 +48,9 @@ router.get('/', function (req, res, next) {
 
         let skip = (data.page - 1) * data.limit;
 
-        return Content.find().limit(data.limit).skip(skip).populate(['category', 'user']);
+        return Content.where(where).find().limit(data.limit).skip(skip).populate(['category', 'user']).sort({
+            addTime: -1
+        });
 
     }).then(function (contents) {
         data.contents = contents;
